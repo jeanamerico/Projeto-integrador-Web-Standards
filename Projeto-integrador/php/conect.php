9d3ba22306b8b3355514f6cd85ac188f1c79c8a8
Jean Américo <47488217+jeanamerico@users.noreply.github.com>
2020-09-22 16:18:52 -0300

    Add files via upload

---
+++ Projeto-integrador/php/conect.php
@@ -0,0 +1,20 @@
+<?php
+
+$servidor= "localhost";
+$usuario= "root";
+$senha = "";
+$dbname= "projeto_integrador";
+
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+
+$nome = filter_input(INPUT_POST, 'nome');
+$sobrenome = filter_input(INPUT_POST, 'snome');
+$email = filter_input(INPUT_POST, 'email');
+$msg = filter_input(INPUT_POST, 'textarea');
+$tel = filter_input(INPUT_POST, 'telefone');
+
+$inserirbd = "INSERT INTO formulario VALUES ('$nome', '$sobrenome', '$email', '$tel', '$msg')";
+
+$resutadofinal = mysqli_query($conn, $inserirbd);
+
+?>
