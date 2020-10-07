30b94efb462bedd3cb5a061a46e82dc57a6a4b9f
Jean Américo <47488217+jeanamerico@users.noreply.github.com>
2020-10-07 10:00:15 -0300

    Add files via upload

--- Projeto-integrador/php/conect.php
+++ Projeto-integrador/php/conect.php
@@ -13,10 +13,10 @@ $email = filter_input(INPUT_POST, 'email');
 $tel = filter_input(INPUT_POST, 'telefone');
 $msg = filter_input(INPUT_POST, 'textarea');
 $verif = filter_input(INPUT_POST, 'verifica');
-$termuso = filter_input(INPUT_POST, 'termuso');
+$termuso = filter_input(INPUT_POST, 'termouso');
 
-$inserirbd = "INSERT INTO formulario VALUES ('', '$nome', '$sobrenome', '$email', '$tel', '$msg', '$verif', '$termuso' )";
-    $resutadofinal = mysqli_query($conn, $inserirbd);
+$inserirbd = "INSERT INTO formulario VALUES ('', '$nome', '$sobrenome', '$email', '$tel', '$msg', '$verif', '$termuso')";
+$resutadofinal = mysqli_query($conn, $inserirbd);
 
 if(mysqli_insert_id($conn)){
     header("Location: ../html/contato.html");
