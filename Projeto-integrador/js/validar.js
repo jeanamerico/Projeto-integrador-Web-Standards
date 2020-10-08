b81c81882b8d83a3b587860799a1752e44eaeef1
Jean Américo <47488217+jeanamerico@users.noreply.github.com>
2020-10-08 20:29:45 -0300

    Add files via upload

--- Projeto-integrador/js/validar.js
+++ Projeto-integrador/js/validar.js
@@ -1,38 +1,38 @@
 function validar() {
-    /*
+
     var nome = formulario.nome.value;
-    */
     var snome = formulario.snome.value;
     var email = formulario.email.value;
     var telefone = formulario.telefone.value;
     var textarea = formulario.textarea.value;
+    /*
     let os = document.getElementsByName('nome');
    
 
     if(os.length < 3){
-        alert("Informe o seu" + os + "correto!");
+        alert('Informe o seu' + os + 'correto!');
         return false;
     }
-/*
+*/
     if (nome.length < 3 ) {
         alert('Informe seu nome correto!');
         formulario.nome.focus();
         return false;
     }
-*/
+
     if (snome.length < 3) {
         alert('Informe seu sobrenome correto!');
         formulario.snome.focus();
         return false;
     }
 
-    if (email.length < 3) {
+    if (email.indexOf('@') < 0) { /*melhorar*/
         alert('Informe seu email correto!');
         formulario.email.focus();
         return false;
     }
 
-    if (telefone.length < 7) {
+    if (telefone.length < 7) { /*isnan*/
         alert('Informe um telefone valido');
         formulario.telefone.focus();
         return false;
