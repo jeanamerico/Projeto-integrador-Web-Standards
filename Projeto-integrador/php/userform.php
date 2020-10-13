ce4161315604562f4d40f0e312ab6fbf440c6ffe
Jean Américo <47488217+jeanamerico@users.noreply.github.com>
2020-10-13 20:46:36 -0300

    Add files via upload

--- Projeto-integrador/php/userform.php
+++ Projeto-integrador/php/userform.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 $servidor = "localhost";
 $usuario = "root";
 $senha = "";
@@ -6,15 +8,15 @@ $dbname = "projeto_integrador";
 
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
 
-if (empty($_POST['usuario']) || empty($_POST['senha'])) {
-    header('Location: ../html/login.html');
+if (empty($_POST['login']) || empty($_POST['passwd'])) {
+    header('Location: index.php');
     exit();
 }
 
-$usuario = mysqli_real_escape_string($conn, $_POST['usuario']);
-$senha = mysqli_real_escape_string($conn, $_POST['senha']);
+$usuario = mysqli_real_escape_string($conn, $_POST['login']);
+$senha = mysqli_real_escape_string($conn, $_POST['passwd']);
 
-$query = "select usuario from usuario where usuario = '{$usuario}' and senha = md5('{$senha}')";
+$query = "SELECT usuario FROM userform WHERE usuario LIKE '{$usuario}' AND senha = md5('{$senha}')";
 
 $result = mysqli_query($conn, $query);
 
@@ -22,10 +24,10 @@ $row = mysqli_num_rows($result);
 
 if ($row == 1) {
     $_SESSION['usuario'] = $usuario;
-    header('location: penisbonito.html');
+    header('location: ../acertou.html');
     exit();
 } else {
     $_SESSION['Não autenticado'] = true;
-    header('location: ../index.html');
+    header('location: ../errou.html');
     exit();
 }
