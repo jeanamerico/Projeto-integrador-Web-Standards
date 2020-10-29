5ac9cb478041a19e766ccf4fa929f963081e4840
Jean Americo <Jean.americo9898@gmail.com>
2020-10-29 00:42:26 -0300

    master

--- Projeto-integrador/php/admcomp.php
+++ Projeto-integrador/php/admcomp.php
@@ -2,17 +2,19 @@
 include('conectar.php');
 include('verifica.php');
 
-$sql = "SELECT usuario FROM userform";
-$resultado = mysqli_real_escape_string($conn, $sql);
 
-while($verificador = mysqli_fetch_array($sql)){
+$sql = "SELECT usuario,nivel FROM userform";
+$resultado = mysqli_query($conn, $sql);
+$verificador = mysqli_num_rows($resultado);
+
+while ($verificador = mysqli_fetch_array($resultado)) {//vrificar if else imprimindo as 2 coisas
     $adm = $verificador['nivel'];
-    if($adm = 'admin'){
+    if ($adm == "Admin") {
         echo '
         <li><a class="tabnav" href="deletar.php">Excluir usuario</a></li>
         <li><a class="tabnav" href="cadastro.php">Cadastrar usuario</a></li>
         ';
-    }else{
+    } else {
         echo '
         <li><a class="tabnav" href="../index.html">Home</a></li>
         <li><a class="tabnav" href="../html/servicos.html">Serviços</a></li>
--- Projeto-integrador/php/userform.php
+++ Projeto-integrador/php/userform.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-
 include('conectar.php');
 
 if (empty($_POST['login']) || empty($_POST['passwd'])) {
