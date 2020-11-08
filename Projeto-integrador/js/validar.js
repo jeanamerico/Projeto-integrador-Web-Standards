89f227ec448b0720fb1fe11b220a25a4260e84a5
Jean Americo <Jean.americo9898@gmail.com>
2020-11-08 01:10:49 -0300

    master

--- Projeto-integrador/js/validar.js
+++ Projeto-integrador/js/validar.js
@@ -1,7 +1,3 @@
-/*
-fazer sistema de coleta de dados e localização em php
-*/
-
 function somenteNumeros(num) {
     var er = /[^0-9.]/;
     er.lastIndex = 0;
@@ -11,25 +7,18 @@ function somenteNumeros(num) {
     }
 }
 
+function apenasnome(name){//adicionar o regex para cancelar {}<>
+    var nome = / /;
+    nome.lastIndex = 0;
+    var campo = name;
+    if (nome.test(campo.value)){
+        campo.value = "";
+    }
+}
 
 function validar() {
-    var nome = formulario.nome.value;
-    var snome = formulario.snome.value;
     var email = formulario.email.value;
     var telefone = formulario.telefone.value;
-    var textarea = formulario.textarea.value;
-
-    if (nome.length < 3) {
-        alert('Informe seu nome');
-        formulario.nome.focus();
-        return false;
-    }
-
-    if (snome.length < 3) {
-        alert('Informe seu sobrenome correto!');
-        formulario.snome.focus();
-        return false;
-    }
 
     if (email.indexOf('@') < 0 || email.indexOf('.com') == -1 ) {
         alert('Informe seu email correto!');
@@ -40,12 +29,6 @@ function validar() {
     if (telefone.length < 7) {
         alert('Informe um telefone valido');
         formulario.telefone.focus();
-        return false;
-    }
-
-    if (textarea.length <= 10) {
-        alert('Digite uma mensagem com o minimo de 10 caracteres!');
-        formulario.textarea.focus();
         return false;
     }
 
