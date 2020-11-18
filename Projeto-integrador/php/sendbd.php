e679dc14a9804e7a226e7418212b777711f08f7a
Jean Americo <Jean.americo9898@gmail.com>
2020-11-18 15:39:34 -0300

    passwd improvement

--- Projeto-integrador/php/sendbd.php
+++ Projeto-integrador/php/sendbd.php
@@ -7,7 +7,7 @@ $nivel = filter_input(INPUT_POST, 'verifica');
 
 
 if($nivel == 'Admin' || 'Comum') {
-    $inserirbd = "INSERT INTO userform VALUES ('', '$login', md5('$senha'), '$nivel', '0')";
+    $inserirbd = "INSERT INTO userform VALUES ('', '$login', SHA2('$senha', 256), '$nivel', '0')";
     $resutadofinal = mysqli_query($conn, $inserirbd);
     if(mysqli_insert_id($conn)){
     header("Location: ../sistemaphp/sistema.php");
